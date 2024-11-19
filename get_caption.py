80e7dca6c2f051cc31d729a30883286d7a0bba62
Shweata <shweata.hegde@gmail.com>
2024-11-19 11:13:30 +0530

    cleaned up code

--- get_caption.py
+++ get_caption.py
@@ -1,12 +1,20 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import TextFormatter
 
-transcript = YouTubeTranscriptApi.get_transcript('zBkVCpbNnkU')
+def get_caption_as_text(video_id):
+    transcript = YouTubeTranscriptApi.get_transcript(video_id)
+    formatter = TextFormatter()
+    text_formatted_transcript = formatter.format_transcript(transcript)
+    return text_formatted_transcript
 
-formatter = TextFormatter()
+def write_to_text_file(text_formatted_transcript, name_of_file):
+    with open(name_of_file, 'w', encoding='utf-8') as txt_file:
+        txt_file.write(text_formatted_transcript)
+        print(f"caption written to: {name_of_file}")
 
-# .format_transcript(transcript) turns the transcript into a JSON string.
-text_formatted_transcript = formatter.format_transcript(transcript)
-
-with open('example-transcript.txt', 'w', encoding='utf-8') as txt_file:
-    txt_file.write(text_formatted_transcript)
+def main():
+    text_formatted_transcript = get_caption_as_text('zBkVCpbNnkU')
+    write_to_text_file(text_formatted_transcript, "example-caption.txt")
+    
+if __name__ == "__main__":
+    main()
